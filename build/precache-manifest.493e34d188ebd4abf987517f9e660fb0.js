009fe3c11120cdbf123b0a6c7b07a31eaf3278db
marcelodeus98 <marcelodeus98@gmail.com>
2022-07-03 07:51:41 -0300

    add project in section project

---
+++ build/precache-manifest.493e34d188ebd4abf987517f9e660fb0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e2290139bcf6f8cba82",
+    "url": "/My_Portfolio/static/css/main.3b12dbed.chunk.css"
+  },
+  {
+    "revision": "8e2290139bcf6f8cba82",
+    "url": "/My_Portfolio/static/js/main.b0822bc2.chunk.js"
+  },
+  {
+    "revision": "578ce61cda3234350f52",
+    "url": "/My_Portfolio/static/js/runtime~main.df9bda23.js"
+  },
+  {
+    "revision": "9e31d19f7bdd8c26c0f8",
+    "url": "/My_Portfolio/static/js/2.58ecc6e4.chunk.js"
+  },
+  {
+    "revision": "b5b6dc3c8ccc95c3ea65c9925c0a4505",
+    "url": "/My_Portfolio/static/media/image-main.b5b6dc3c.jpg"
+  },
+  {
+    "revision": "a27d0a0f524c2083f07cf9f70956371d",
+    "url": "/My_Portfolio/static/media/logo.a27d0a0f.png"
+  },
+  {
+    "revision": "5a95f1cccba741df3618a7c0bcbbc6d5",
+    "url": "/My_Portfolio/static/media/perfil-portfolio.5a95f1cc.jpg"
+  },
+  {
+    "revision": "a1098276d015d05bf7be1e557a5a38e9",
+    "url": "/My_Portfolio/static/media/project-portfolio.a1098276.png"
+  },
+  {
+    "revision": "44490232f652c2ce182cf864f57058f4",
+    "url": "/My_Portfolio/static/media/project1.44490232.png"
+  },
+  {
+    "revision": "5408ef8cb4dd7a83e958280ba0dffbc6",
+    "url": "/My_Portfolio/static/media/project2.5408ef8c.jpg"
+  },
+  {
+    "revision": "eafadf30771ef62b2886ebe391cfc53b",
+    "url": "/My_Portfolio/static/media/project3.eafadf30.jpeg"
+  },
+  {
+    "revision": "4963436b571fb7f19c81c4e3ef3bd3c7",
+    "url": "/My_Portfolio/static/media/img-c5.4963436b.png"
+  },
+  {
+    "revision": "01f401cb4a88caa5817e733dcf463f97",
+    "url": "/My_Portfolio/static/media/img-c3.01f401cb.jpeg"
+  },
+  {
+    "revision": "1c4be48bc13eee630d7c8745e24fd03e",
+    "url": "/My_Portfolio/static/media/img-c4.1c4be48b.jpeg"
+  },
+  {
+    "revision": "7fd665f9f6fe8636a5bb52338785d9b6",
+    "url": "/My_Portfolio/static/media/img-c2.7fd665f9.jpeg"
+  },
+  {
+    "revision": "ad6b09103ffca3644d0a242f48fa8a0f",
+    "url": "/My_Portfolio/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/My_Portfolio/precache-manifest.493e34d188ebd4abf987517f9e660fb0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/My_Portfolio/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
